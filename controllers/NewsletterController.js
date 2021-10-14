91dcee07435c4a929a2fccf2be30d6b31d647f16
Deepak Bawa <deepak4bawa@gmail.com>
2021-10-14 23:52:51 +0530

    working on newsletter subscription

--- controllers/NewsletterController.js
+++ controllers/NewsletterController.js
@@ -64,7 +64,7 @@ module.exports = class NewsletterController {
                                     })
                                     .catch(async (ex) => {
                                         console.log('exceptio occur:', ex);
-                                        // return await responseServiceObj.sendException(res, {msg: ex.toString()});
+                                        return await responseServiceObj.sendException(res, {msg: ex.toString()});
                                     });
 
                             // return await responseServiceObj.sendResponse(res, {
