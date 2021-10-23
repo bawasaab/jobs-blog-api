6359d35b06a80d17ba86515f8db3ec729660d970
Deepak Bawa <bawa_d@ymail.com>
2021-10-23 14:15:44 +0530

    working

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+const ArticleService = require('./services').ArticleService;
+const articleServiceObj = new ArticleService();
+
 var indexRouter = require('./routes/index');
 
 var app = express();
